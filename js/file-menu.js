7f31f4c48ca9bdaa0bf004a2560b5733f4e6d7f0
Crystal Qian <crystaljqian@gmail.com>
2016-02-03 00:31:58 -0500

    upload image

---
+++ js/file-menu.js
@@ -0,0 +1,30 @@
+//upload image and perform triangulation
+window.onload = function() {
+
+    var fileInput = document.getElementById('import-images');
+    var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+
+    fileInput.addEventListener('change', function(e) {
+        var file = fileInput.files[0];
+        var imageType = /image.*/;
+
+        if (file.type.match(imageType)) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                var img = new Image();
+                img.src = reader.result;
+
+                //send the image to the triangulation station
+                triangulateImage(img);
+            }
+
+            reader.readAsDataURL(file); 
+        } else {
+            fileDisplayArea.innerHTML = "File not supported!"
+        }
+    });
+
+}
+
---
+++ js/imageparser.js
@@ -0,0 +1,11 @@
+function triangulateImage(img){
+    console.log(img);
+
+    sigma.parsers.json('data/data.json', {
+        container: 'canvas-container',
+        settings: {
+            defaultNodeColor: '#ec5148'
+        }
+    });
+
+}
